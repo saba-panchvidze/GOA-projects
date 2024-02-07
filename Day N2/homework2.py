a64f752874ae46155f745a42d1d568da1410a98d
saba-panchvidze <saba.panchvidze.3@gmail.com>
2024-02-07 12:06:00 +0400

    second commit

---
+++ Day N2/homework2.py
@@ -0,0 +1,8 @@
+name = "saba" 
+surname = "panchvidze" 
+age = 20 
+height = 182 
+weight = 95 
+living_place = "tbilisi"
+
+print(name +" "+ surname +" "+ str(age) +" "+ str(height) +" "+ str(weight) +" "+ living_place)
---
+++ Day N2/lesson2.py
@@ -0,0 +1,34 @@
+name = "saba" 
+#name არის ცვლადი
+# = არის ცვლადის მნიშვნელობის მიმნჭებელი სიმბოლო
+# "saba" არის ცვლადისთვის მნიშვნელობა
+
+surname = "panchvidze"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "saba"   #ეს არის str (string) ტიპის ცვლადი
+age = 20 # ეს არის int ( integer ) მთელი რიცხვი 
+height = 182 #ეს არის float ტიპის ცვლადი ( ათწილადი) 
+#boolean (bool) ტიპის ცვლადი 
+
+knows_programming = False #True ან false 
+is_ugly = False 
+
+isUgly = False #ჯავასკრიპტული camelcase 
+
+
+print(name +" "+ surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age)) 
+#print(type(name)) 
+#print(type(surname)) 
+#print(type(height)) 
+#print(type(knows_programming)) 
+
+print(name +" "+ str(age)) 
+
