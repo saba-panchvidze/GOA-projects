bbcfecb277b9f9b5cd371acd99aceea4d71ed5b7
saba-panchvidze <saba.panchvidze.3@gmail.com>
2024-02-17 14:05:49 +0400

    third commit

---
+++ Day N03/homework.py
@@ -0,0 +1,13 @@
+mom_name = "xatia " 
+mom_surname = "gogoladze" 
+
+father_name = "bondo" 
+father_surname = "panchvidze" 
+
+sister_name = "nini" 
+sister_surname = "panchvidze" 
+
+
+print (mom_name,mom_surname)
+print(father_name, father_surname)
+print(sister_name, sister_surname)
---
+++ Day N04/homework.py
@@ -0,0 +1,6 @@
+my_name = "saba"
+my_surname = "panchvidze" 
+age = 20 
+age + 10 
+
+print (my_name, my_surname, age, age + 10) 
