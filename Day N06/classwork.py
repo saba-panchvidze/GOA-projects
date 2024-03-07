cfe6be410432ec25dda1f879a362f5245e4155aa
saba-panchvidze <saba.panchvidze.3@gmail.com>
2024-03-07 19:57:18 +0400

    forth commit

---
+++ Day N06/classwork.py
@@ -0,0 +1,23 @@
+name = "saba"
+print(name)
+
+name = input("what is your name: ") 
+print("your name is " + name)  
+
+age = input("what is your age") 
+print("your age is " + age) 
+
+
+num1 = int(input("your_number1: "))
+num2 = int(input("your_number2: "))
+num3 = int(input("your_number3: ")) 
+result = (num1 * num2* num3) 
+print(result) 
+
+
+name = input("please enter your name: ")
+lastname = input("please enter your lasname: ") 
+age = input("please enter your age: ") 
+introduction = "hello, i am " + name + " " + lastname + " " + "i am" + " "+ age +" " + "years old" 
+
+print(introduction) 
---
+++ Day N07/classwork.py
@@ -0,0 +1,11 @@
+# logical AND operator
+print(True and False) # Result = False 
+print(True and True) # Result = True
+print(False and True) # Result = False
+print(False and False) # Result = False 
+
+# logical OR operator
+print(True or False) # Result = True 
+print(True or True) # Result = True
+print(False or True) # Result = True
+print(False or False) # Result = False 
